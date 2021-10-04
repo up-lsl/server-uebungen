1a0fd1fe55ce021c5644008b6576e6e364819e7a
Thom Fri <thom.fri@gmx.de>
2021-10-04 15:02:10 +0200

    LAMP: Kleine Verbesserungen und Vereinheitlichungen

--- webapps/simple-dashboard/lamp/build_data_table.php
+++ webapps/simple-dashboard/lamp/build_data_table.php
@@ -1,43 +1,48 @@
 <?php
-//Datenbankzugriffe werden in data.php verwaltet.
-include_once "data.php";
+//Datenbankzugriffe werden in api.php verwaltet.
+include_once "api.php";
 
 //Hier werden HTML-Tabellen zum Anzeigen von Datenbankinhalten erstellt.
 
 //Erstellt eine Tabelle aus Kundendaten aus der Tabelle mit dem übergebenen Namen.
-function erstelle_tabelle_kunden($name_tabelle){
+function erstelle_tabelle_kunden($name_tabelle) {
 	//Frage benötigte Daten aus Datenbank ab.
 	$data = get_data($name_tabelle);
 	
 	//Ergebnis afbereiten.
 	print "<table class='datatable'>";
 	//Tabellenkopf erzeugen.
-	print "<tr>";
-	print "<th> ID </th>";
-	print "<th> Name </th>";
-	print "</tr>";
+	print "	<thead>";
+	print "		<tr>";
+	print "			<th> ID </th>";
+	print "			<th> Name </th>";
+	print "		</tr>";
+	print "	</thead>";
 	//Hier werden die einzelnen Zeilen des enmpfangenen Datensatzes durchlaufen.
+	print "	<tbody>";
 	foreach ($data as $row) {
 		//Zeile ausgeben.
-		print "<tr>";
+		print "	<tr>";
 		//Strings können mit '.' zusammengefügt werden.
 		//Dies ist vor allem dann nützlich wenn mit Variablen gearbeitet wird.
-		print "<td>".$row["id"]."</td>";
-		print "<td>".$row['name']."</td>";
-		print "</tr>";
+		print "		<td>".$row["id"]."</td>";
+		print "		<td>".$row['name']."</td>";
+		print "	</tr>";
 	}
-	//Füge eine Zeile hinzu, um die Eingabe weiterer Kundendaten zu ermöglichen.
-	print "<tr>";
 	//Ein Forumlar kann mithilfe von 'action' direkt Anfragen an ein PHP-Skript stellen.
-	print "<form action='data.php' method='post'>";
+	print "		<form action='api.php' method='post'>";
+	//Füge eine Zeile hinzu, um die Eingabe weiterer Kundendaten zu ermöglichen.
+	print "			<tr>";
 	//Die eingegebenen Werte aus den input feldern werden dabei der Anfrage hinzugefügt.
-	print "<td> <input type='number' name='id'> </td>";
-	print "<td> <input type='text' name='name'> </td>";
-	print "<input type='hidden' name='type' value='kundentabelle' />";
-	print "</tr>";
+	print "				<td> <input type='number' name='id'> </td>";
+	print "				<td> <input type='text' name='name'> </td>";
+	print "			</tr>";
+	print "			<tr>";
 	//Dazu wird das input-Element mit type='submit' benötigt, um die Anfrage zu versenden.
-	print "<input type='submit'>";
-	print "</form>";
+	print "				<td colspan='2'> <input type='submit'> </td>";
+	print "			</tr>";
+	print "		</form>";
+	print "	</tbody>";
 	print "</table>";
 }
 
@@ -49,22 +54,26 @@ function erstelle_tabelle_produkte($name_tabelle){
 	//Ergebnis aufbereiten.
 	print "<table class='datatable'>";
 	//Tabellenkopf erzeugen.
-	print "<tr>";
-	print "<th> Name </th>";
-	print "<th> Verfügbar </th>";
-	print "</tr>";
+	print "	<thead>";
+	print "		<tr>";
+	print "			<th> Name </th>";
+	print "			<th> Verfügbar </th>";
+	print "		</tr>";
+	print "	</thead>";
+	print "	<tbody>";
 	foreach ($data as $row) {
 		//Zeile ausgeben.
-		print "<tr>";
-		print "<td>".$row['name']."</td>";
-		print "<td class='wide'>";
+		print "	<tr>";
+		print "		<td>".$row['name']."</td>";
+		print "		<td class='wide'>";
 		//Füge Buttons hinzu, um den Lagerbestand zu senken, bzw. zu erhöhen.
-		print "<button class='decrease' pid=".$row["id"].">-</button>";
-		print "<div pid=".$row["id"].">".$row['bestand']."</div>";
-		print "<button class='increase' pid=".$row["id"].">+</button>";
-		print"</td>";
-		print "</tr>";
+		print '         <button onclick="reduziereBestand('.$row["id"].')">-</button>';
+		print '         <div id=bestand_'.$row["id"].'>'.$row["bestand"].'</div>';
+		print '         <button onclick="erhoeheBestand('.$row["id"].')">+</button>';
+		print"		</td>";
+		print "	</tr>";
 	}
+	print "	</tbody>";
 	print "</table>";
 }
 ?>
