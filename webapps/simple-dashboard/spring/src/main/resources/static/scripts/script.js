ea7e26423e1762c0d975309fe4567f5755428a48
Thom Fri <thom.fri@gmx.de>
2021-10-17 12:18:22 +0200

    Typos fixed

--- webapps/simple-dashboard/spring/src/main/resources/static/scripts/script.js
+++ webapps/simple-dashboard/spring/src/main/resources/static/scripts/script.js
@@ -22,7 +22,7 @@ function getBestandFromPage(produkt_id){
 
 // Funktion, die die API zur Aktualisierung des Bestands aufruft und auch die Anzeige auf der Webseite verändert
 function aktualisiereBestand(produkt_id, bestand) {
-	//Erzeuge AJAX-Anfrage:
+	// Erzeuge AJAX-Anfrage:
 	let ajaxurl = 'api/updateBestand/'+produkt_id,
 		data = "bestand="+bestand;
 
@@ -30,14 +30,13 @@ function aktualisiereBestand(produkt_id, bestand) {
 	request.onreadystatechange = function () {
 		if (request.readyState == 4 && request.status == 200) {
 			console.log(request.responseText);
-			//Verringere die Anzahl auf dem Bildschirm um 1,
-			//nachdem die Server-Anfrage ausgeführt wurde.
+			// Verringere die Anzahl auf dem Bildschirm um 1, nachdem die Server-Anfrage ausgeführt wurde.
 			let bestand_html_element = document.querySelector('div[id="bestand_'+produkt_id+'"]');
 			bestand_html_element.innerHTML = bestand;
 		}
 	};
 
-	//Führe Anfrage aus.
+	// Führe Anfrage aus.
 	request.open('POST', ajaxurl, true);
 	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 	request.send(data);
