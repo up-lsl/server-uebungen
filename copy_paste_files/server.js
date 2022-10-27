2334f4a426f56fb7e39d08e353bb62301d5ae794
ThomFri <36042923+ThomFri@users.noreply.github.com>
2022-10-27 12:14:59 +0200

    Delete server.js

--- copy_paste_files/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  fs.createReadStream('test.html').pipe(res); })
-
-server.listen(3000, "0.0.0.0");
-console.log('Server running at port 3000!');
